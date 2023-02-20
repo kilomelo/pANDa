8911a7ff9ac702264164ebf128670fd3a60215db
Kilomelo <weichuyoucai@gmail.com>
2023-02-20 22:46:24 +0800

    test

--- app/src/main/java/com/kilomelo/panda/overlay/FloatButton.java
+++ app/src/main/java/com/kilomelo/panda/overlay/FloatButton.java
@@ -6,6 +6,8 @@ import android.view.Gravity;
 import android.view.ViewGroup;
 
 import com.hjq.xtoast.XToast;
+import com.hjq.xtoast.draggable.MovingDraggable;
+import com.hjq.xtoast.draggable.SpringDraggable;
 import com.kilomelo.panda.MainActivity;
 import com.kilomelo.panda.R;
 import com.kilomelo.tools.LogTool;
@@ -14,14 +16,14 @@ public class FloatButton extends XToast {
     private static String TAG = FloatButton.class.getSimpleName();
 
     private static int OPERATION_PANEL_DURATION = 5000;
-    private OperationPanel mOperationPanel;
+//    private OperationPanel mOperationPanel;
     enum State
     {
         collapsed,
         expanded,
     }
 
-    private FloatButtonDraggable mDraggable;
+//    private SpringDraggable mDraggable;
     private State mState;
 
     public FloatButton(Application application) {
@@ -36,9 +38,9 @@ public class FloatButton extends XToast {
 
         setGravity(Gravity.TOP | Gravity.START);
         // 设置指定的拖拽规则
-        mDraggable = new FloatButtonDraggable();
-        mDraggable.deserializeLocation();
-        setDraggable(mDraggable);
+//        SpringDraggable mDraggable = new SpringDraggable();
+//        mDraggable.deserializeLocation();
+        setDraggable(new MovingDraggable());
         mState = State.collapsed;
     }
 
@@ -46,7 +48,7 @@ public class FloatButton extends XToast {
     public void show() {
         super.show();
         LogTool.logMethod();
-        if (null != mDraggable) mDraggable.relocate();
+//        if (null != mDraggable) mDraggable.relocate();
 
         ViewGroup decorView = (ViewGroup)getDecorView();
         if (null == decorView)
@@ -58,52 +60,53 @@ public class FloatButton extends XToast {
     @Override
     public void cancel() {
         LogTool.logMethod();
-        mDraggable = null;
-        if (null != mOperationPanel) mOperationPanel.cancel();
-        mOperationPanel = null;
+//        mDraggable = null;
+//        if (null != mOperationPanel) mOperationPanel.cancel();
+//        mOperationPanel = null;
         super.cancel();
     }
     //region business
-    private void showOperationPanel(boolean show) {
-        LogTool.logMethod();
-        if (show) {
-            if (null != mOperationPanel)
-            {
-                Log.e(TAG, "mOperationPanel is not null, perhaps mem leak.");
-                return;
-            }
-            mOperationPanel = new OperationPanel(MainActivity.getInstance().getApplication());
-            mOperationPanel.setDuration(OPERATION_PANEL_DURATION).setOnToastLifecycle(new OnLifecycle() {
-                @Override
-                public void onDismiss(XToast<?> toast) {
-                    OnLifecycle.super.onDismiss(toast);
-                    mOperationPanel = null;
-                    mDraggable.mEnableDrag = true;
-                }
-            }).show();
-            mDraggable.mEnableDrag = false;
-        } else {
-            if (null == mOperationPanel)
-            {
-                Log.e(TAG, "mOperationPanel is null");
-                return;
-            }
-            else {
-                mOperationPanel.cancel();
-                mOperationPanel = null;
-                mDraggable.mEnableDrag = true;
-            }
-        }
-    }
+//    private void showOperationPanel(boolean show) {
+//        LogTool.logMethod();
+//        if (show) {
+//            if (null != mOperationPanel)
+//            {
+//                Log.e(TAG, "mOperationPanel is not null, perhaps mem leak.");
+//                return;
+//            }
+//            mOperationPanel = new OperationPanel(MainActivity.getInstance().getApplication());
+//            mOperationPanel.setDuration(OPERATION_PANEL_DURATION).setOnToastLifecycle(new OnLifecycle() {
+//                @Override
+//                public void onDismiss(XToast<?> toast) {
+//                    OnLifecycle.super.onDismiss(toast);
+//                    mOperationPanel = null;
+//                    mDraggable.mEnableDrag = true;
+//                }
+//            }).show();
+//            mDraggable.mEnableDrag = false;
+//        } else {
+//            if (null == mOperationPanel)
+//            {
+//                Log.e(TAG, "mOperationPanel is null");
+//                return;
+//            }
+//            else {
+//                mOperationPanel.cancel();
+//                mOperationPanel = null;
+//                mDraggable.mEnableDrag = true;
+//            }
+//        }
+//    }
     public void ToggleOperationPanel()
     {
         LogTool.logMethod();
-        showOperationPanel(null == mOperationPanel);
+//        showOperationPanel(null == mOperationPanel);
     }
     public void awakeMainWindow()
     {
         LogTool.logMethod();
-        if (null == mOperationPanel) MainActivity.getInstance().moveToFront();
+//        if (null == mOperationPanel)
+            MainActivity.getInstance().moveToFront();
     }
     //endregion
 }
